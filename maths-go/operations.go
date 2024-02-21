d25ec1e721f4cedd9851abc1350b8bd9287a2262
Abdelilah SADIQUI <143453064+sadiqui@users.noreply.github.com>
2024-02-21 18:43:21 +0100

    no need to float values

--- maths-go/operations.go
+++ maths-go/operations.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func operations(x, y float64) (float64, float64, float64, float64, error) {
+func returnmath(x, y float64) (float64, float64, float64, float64, error) {
 	sum := x + y
 	dif := x - y
 	pro := x * y
@@ -13,10 +13,27 @@ func operations(x, y float64) (float64, float64, float64, float64, error) {
 	return sum, dif, pro, quo, nil
 }
 
+func printmath(x, y float64) {
+	sum := x + y
+	dif := x - y
+	pro := x * y
+	quo := x / y
+	err := "division by zero!"
+	fmt.Println("Results of", x, "&", y)
+	fmt.Println("Sum:", sum)
+	fmt.Println("Difference:", dif)
+	fmt.Println("Product:", pro)
+	if y != 0 {
+		fmt.Println("Quotient:", quo)
+	} else {
+		fmt.Println("Quotient:", err)
+	}
+}
+
 func main() {
 	a, b := 10, 2
-	sum1, dif1, pro1, quo1, err1 := operations(float64(a), float64(b))
-	fmt.Println("Arithmetic Results")
+	sum1, dif1, pro1, quo1, err1 := returnmath(float64(a), float64(b))
+	fmt.Println("Results of", a, "&", b)
 	fmt.Println("Sum:", sum1)
 	fmt.Println("Difference:", dif1)
 	fmt.Println("Product:", pro1)
@@ -29,26 +46,44 @@ func main() {
 	fmt.Println()
 
 	A, B := 10.0, 0.0
-	sum2, dif2, pro2, quo2, err2 := operations(A, B)
-	fmt.Println("Arithmetic Results")
+	sum2, dif2, pro2, quo2, err2 := returnmath(A, B)
+	fmt.Println("Results of", A, "&", B)
 	fmt.Println("Sum:", sum2)
 	fmt.Println("Difference:", dif2)
 	fmt.Println("Product:", pro2)
 	if err2 == nil {
 		fmt.Println("Quotient:", quo2)
 	} else {
-		fmt.Println("Quotient: error,", err2)
+		fmt.Println("Quotient:", err2)
 	}
+
+	fmt.Println()
+	printmath(15, 3)
+
+	fmt.Println()
+	printmath(15, 0)
 }
 
-// Arithmetic Results
+// Results of 10 & 2
 // Sum: 12
 // Difference: 8
 // Product: 20
 // Quotient: 5
 
-// Arithmetic Results
+// Results of 10 & 0
 // Sum: 10
 // Difference: 10
 // Product: 0
-// Quotient: error, division by zero!
+// Quotient: division by zero!
+
+// Results of 15 & 3
+// Sum: 18
+// Difference: 12
+// Product: 45
+// Quotient: 5
+
+// Results of 15 & 0
+// Sum: 15
+// Difference: 15
+// Product: 0
+// Quotient: division by zero!
